506986944b7f0e492a2b85f9f001d731d37af8ae
mozAndrey <3402391995aZ>
2022-04-23 17:58:54 +0300

    1

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -2,6 +2,7 @@ package jm.task.core.jdbc;
 
 import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.service.UserServiceImpl;
 import jm.task.core.jdbc.util.Util;
 
 import java.sql.*;
@@ -10,17 +11,16 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        UserDaoJDBCImpl userDaoJDBC = new UserDaoJDBCImpl();
-        userDaoJDBC.createUsersTable();
-        userDaoJDBC.saveUser("John","Sina", (byte) 26);
-        userDaoJDBC.saveUser("Jackie","Chan", (byte) 27);
-        userDaoJDBC.saveUser("Andrey","Mozal", (byte) 36);
-        userDaoJDBC.saveUser("Jack","Salt", (byte) 45);
-        List<User> list = userDaoJDBC.getAllUsers();
-        list.forEach(System.out::println);
-        userDaoJDBC.cleanUsersTable();
-        userDaoJDBC.dropUsersTable();
 
+        UserServiceImpl userService = new UserServiceImpl();
+        userService.createUsersTable();
+        userService.saveUser("Andrey", "Mozal", (byte) 26);
+        userService.saveUser("Dmitriy", "Peskov", (byte) 27);
+        userService.saveUser("Vladislav", "Nesterov", (byte) 35);
+        userService.saveUser("Ekaterina", "Mozal", (byte) 56);
+        userService.getAllUsers().stream().forEach(System.out::println);
+        userService.cleanUsersTable();
+        userService.dropUsersTable();
     }
 }
 
